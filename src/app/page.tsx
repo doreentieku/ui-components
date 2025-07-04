9e993dd15ba4924188c524bd271f711e72c9a6aa
Doreen Tieku <dofosuatieku@gmail.com>
2025-07-04 15:56:53 -0500

    Add sticky scroll

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -14,6 +14,7 @@ import { CardSpotlightDemo } from "@/app/components/CardSpotlightDemo";
 import { FloatingDockDemo } from "@/app/components/FloatingDockDemo";
 import { GridBackgroundDemo } from "@/app/components/GridBackgroundDemo";
 import { SidebarDemo } from "@/app/components/SidebarDemo";
+import { StickyScrollRevealDemo } from "@/app/components/StickyScrollRevealDemo";
 
 export default function Home() {
   return (
@@ -57,6 +58,12 @@ export default function Home() {
             Sidebar
           </h2>
           <SidebarDemo />
+        </section>
+        <section className="text-center">
+          <h2 className="text-3xl font-bold text-white mb-4">
+            Sticky scroll reveal
+          </h2>
+          <StickyScrollRevealDemo />
         </section>
       </main>
     </div>
