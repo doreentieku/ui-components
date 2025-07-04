ac1ebbbbb80333a00eb887138b112967bcfa323b
Doreen Tieku <dofosuatieku@gmail.com>
2025-07-04 14:42:24 -0500

    add text hover effects

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -2,17 +2,22 @@
 
 import dynamic from "next/dynamic";
 
-const DraggableCardDemo = dynamic(() => import("@/app/components/DraggableCardDemo"), {
-  ssr: false,
-});
+const DraggableCardDemo = dynamic(
+  () => import("@/app/components/DraggableCardDemo"),
+  {
+    ssr: false,
+  }
+);
+
+import { TextHoverEffectDemo } from "@/app/components/TextHoverEffectDemo";
 
 export default function Home() {
   return (
     <div className="">
       <main className="min-h-screen bg-white dark:bg-black">
+        <TextHoverEffectDemo />
         <DraggableCardDemo />
       </main>
-
     </div>
   );
 }
